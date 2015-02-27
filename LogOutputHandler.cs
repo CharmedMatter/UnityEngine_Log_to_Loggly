a80d9a594697e4d5288521a2de9e8c9dfdd7c304
CharmedMatter <miketurner@CharmedMatter.com>
2015-02-27 10:21:12 -0600

    initial commit of tools to log useful data from Unity and send them to Loggly

---
+++ LogOutputHandler.cs
@@ -0,0 +1,40 @@
+﻿/// <summary>
+/// Basic controller that takes logs from Unity's debug.log function and sends output to Loggly
+/// </summary>
+/// USAGE: Simply put this script in your scripts folder and it will operate.
+/// Created by Mike Turner of Charmed Matter Games.
+
+using UnityEngine;
+using System.Collections;
+
+public class LogOutputHandler : MonoBehaviour {
+
+	//Create a string to store log level in
+	string level = "";
+	
+
+	//Register the HandleLog function on scene start to fire on debug.log events 
+	void Awake(){
+		Application.RegisterLogCallback(HandleLog);
+	} 
+	
+	
+	//Capture debug.log output, send logs to Loggly
+	public void HandleLog(string logString, string stackTrace, LogType type) {
+	
+		//Initialize WWWForm and store log level as a string
+		level = type.ToString ();
+		var loggingForm = new WWWForm();
+		
+		//Add log message to WWWForm
+		loggingForm.AddField("LEVEL", level);
+		loggingForm.AddField("Message", logString);
+		loggingForm.AddField("Stack_Trace", stackTrace);
+		
+		//Add any game Device MetaData to  
+		loggingForm.AddField("Device_Model", SystemInfo.deviceModel);
+		
+		//Send WWW Form to Loggly
+		var sendLog = new WWW("http://logs-01.loggly.com/inputs/3daf6441-06c4-41bc-955a-72c2585542c6/tag/http/", loggingForm);
+	}
+}
